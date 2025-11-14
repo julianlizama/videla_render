859d0f6149c0aaa4712a0d1228f6d238f7a931bb
Julian Lizama <lizamajulian14gmail.com>
2025-11-14 15:03:11 -0300

    Quito creación automática de superusuario en wsgi

--- videla/wsgi.py
+++ videla/wsgi.py
@@ -9,13 +9,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "videla.settings")
 
-# Ejecutar la creación automática de superusuario (solo en Render)
-try:
-    from .create_superuser import run
-    run()
-except Exception as e:
-    print("\n=== Error creando superusuario automáticamente ===")
-    print(e)
-    print("=== Fin del error ===\n")
-
 application = get_wsgi_application()
